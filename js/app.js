919e8ab9736cb271d7cd8c9521579cad9d34381a
pouya <pouyakhaleghi82@gmail.com>
2025-12-17 20:23:38 +0330

    project done without backend

--- js/app.js
+++ js/app.js
@@ -2,13 +2,26 @@ import {dashboard} from "/SPA%20project/js/pages/dashboard.js";
 import posts from "/SPA%20project/js/pages/posts.js";
 import products from "/SPA%20project/js/pages/products.js";
 
-function router() {
 
+const open_sidebar = document.querySelector(".sidebar");
+const chevron = document.querySelector(".chevron");
+const svgs = document.querySelectorAll("svg");
+
+
+
+
+
+
+
+function navegateRouter(url) {
+    history.pushState(null, null, url)
+}
+
+function router() {
     const routes = [
         {path:'/SPA%20project/index.html', view: dashboard},
         {path:'/SPA%20project/products', view: products },
         {path:'/SPA%20project/posts', view: posts },
-
     ]
 
     const potentialrouts=routes.map((item) => {
@@ -29,21 +42,18 @@ function router() {
     // }
 
     document.querySelector('.content__items').innerHTML = match.route.view()
-
-    console.log(match.route.view())
 }
 
 
-function navegateRouter(url) {
-    history.pushState(null, null, url)
 
-}
+
+
+window.addEventListener("popstate", router)
+
 
 document.addEventListener('DOMContentLoaded', () => {
     router();
 })
-
-window.addEventListener("popstate", router)
 
 
 document.body.addEventListener('click', (e) => {
@@ -53,5 +63,17 @@ document.body.addEventListener('click', (e) => {
         router();
 
     }
+})
+
+
+
+chevron.addEventListener("click", function() {
+    open_sidebar.classList.toggle("mini--sidebar");
+})
+
+svgs.forEach(svg => {
+    svg.addEventListener("click", function(e) {
+        e.preventDefault();
+    })
 })
 
